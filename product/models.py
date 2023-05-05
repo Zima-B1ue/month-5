fa341391927c5b788e4a20e283ae5bbd4c7a3bc2
Zima-B1ue <123304236+Zima-B1ue@users.noreply.github.com>
2023-05-05 12:00:08 +0300

    hw6

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Category(models.Model):
@@ -8,11 +9,16 @@ class Category(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     price = models.IntegerField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return self.title
@@ -20,7 +26,14 @@ class Product(models.Model):
 
 class Review(models.Model):
     text = models.TextField(null=True, blank=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    stars = models.IntegerField(blank=True, validators=[MaxValueValidator(5), MinValueValidator(1)], default=1)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_reviews')
 
     def __str__(self):
         return self.text
+
+    # def product_reviews(self):
+    #     if self.product:
+    #         return self.text , self.stars
+    #     else:
+    #         return f"No reviews here"
